83217df0d8f83437bb249345665974b0fdfd91eb
isabella-projects <damyan4oy@gmail.com>
2024-05-27 16:43:27 +0200

    Added header component Some small changes to the structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { HeaderComponent } from './header/header.component';
+
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
+    imports: [HeaderComponent],
 })
 export class AppComponent {}
