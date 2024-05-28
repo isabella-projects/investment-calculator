e903c44af08b1822b464db999a1c8081c84819e1
isabella-projects <damyan4oy@gmail.com>
2024-05-28 12:11:13 +0200

    Implemented header and user-input components Handling the form submission Extracting values with Two-Way-Binding (ngModel) Cross-Component Communication with Outputs

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InvestmentInput } from '../investment-input.model';
+
+@Component({
+    selector: 'app-user-input',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './user-input.component.html',
+    styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+    @Output() calculate = new EventEmitter<InvestmentInput>();
+
+    enteredInitialInvestment: string = '0';
+    enteredAnnualInvestment: string = '0';
+    enteredExpectedReturn: string = '5';
+    enteredDuration: string = '0';
+
+    onSubmit() {
+        console.log('submittedd');
+        this.calculate.emit({
+            initialInvestment: +this.enteredInitialInvestment,
+            annualInvestment: +this.enteredAnnualInvestment,
+            expectedReturn: +this.enteredExpectedReturn,
+            duration: +this.enteredDuration,
+        });
+    }
+}
